5b313f9faa70723d935ea894a70fb4304c181231
MaxHallenberger <maximilian.hallenberger@emr.rwth-aachen.de>
2023-03-15 16:24:36 +0100

    - Initial commit of creation feedback gui

---
+++ XRGis/Assets/LODCesium/Terranigma/Runtime/GUI/LODCreationFeedback.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityMeshSimplifier.Plugins.leHighPerformanceMeshSimplifier.MeshSimplifierSingle;
+
+namespace LODCesium.Terranigma.Runtime.GUI
+{
+    public class LODCreationFeedback : MonoBehaviour
+    {
+        public TextMeshProUGUI loadedLODText;
+        public TextMeshProUGUI timeToLoadLODText;
+        
+        private void Update()
+        {
+            loadedLODText.text = $"Currently loading LOD: {LODGenerator.count}";
+            //timeToLoadLODText.text = $"Time to load LOD{}: {}";
+        }
+    }
+}
