8d332fdb55b9b21d0b9e41398d0031f4d7db20d6
MaxHallenberger <maximilian.hallenberger@emr.rwth-aachen.de>
2023-04-27 09:33:15 +0200

    - Changed main. Now the initial state is set in the first frame

--- XRGis/Assets/NurGIS/Runtime/Main.cs
+++ XRGis/Assets/NurGIS/Runtime/Main.cs
@@ -2,38 +2,39 @@ using System.Collections.Generic;
 using NurGIS.Runtime.Geolocation;
 using NurGIS.Runtime.ScanLoading;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace NurGIS.Runtime
 {
     public class Main : MonoBehaviour
     {
+        public bool isInitialized;
         private List<GameObject> _goList;
         public bool instantiateScans;
         public bool reuseScans;
         public bool geoReference;
 
 
-        private void Awake()
-        {
-            if (instantiateScans) // Instantiates scans
-            {
-                _goList = InstantiateFromList.InstantiateAllFromList();
-            }
-            
-            if (geoReference) // Spatial reference is set
-            {
-                GeolocationInterface.GeoReference(_goList);
-            }
-
-            
-            if (reuseScans) // Reuses scans
-            {
-                _goList = ScanLoadingMultiplication.InstantiateMultiplication(_goList);
-            }
-        }
-        
         private void Update()
         {
+            if (!isInitialized)
+            {
+                isInitialized = true;
+                if (instantiateScans) // Instantiates scans
+                {
+                    _goList = InstantiateFromList.InstantiateAllFromList();
+                }
+            
+                if (geoReference) // Spatial reference is set
+                {
+                    GeolocationInterface.GeoReference(_goList);
+                }
+            
+                if (reuseScans) // Reuses scans
+                {
+                    _goList = ScanLoadingMultiplication.InstantiateMultiplication(_goList);
+                }
+            }
         }
     }
 }
