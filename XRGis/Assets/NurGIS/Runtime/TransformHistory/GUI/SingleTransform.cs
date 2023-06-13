3ea800294c045d2219b91597b5995fc0f6ca0015
MaxHallenberger <maximilian.hallenberger@emr.rwth-aachen.de>
2023-06-13 11:54:58 +0200

    [GUI Integration] Miguels visual elements now align nicely

--- XRGis/Assets/NurGIS/Runtime/TransformHistory/GUI/SingleTransform.cs
+++ XRGis/Assets/NurGIS/Runtime/TransformHistory/GUI/SingleTransform.cs
@@ -15,70 +15,112 @@ namespace NurGIS.Runtime.TransformHistory.GUI
 
         private SingleTransform()
         {
-            {
-                m_foldout = new Foldout
+            ///////////////// Single Transform Foldout /////////////////
+            m_foldout = new Foldout
                 {
                     text = "Transform"
                 };
-                hierarchy.Add(m_foldout);
-            }
-            
+            hierarchy.Add(m_foldout);
+            ///////////////// Single Transform Foldout /////////////////
+
+            ///////////////// Vector3Fields /////////////////
+            var position = new Vector3Field
             {
-                var position = new Vector3Field
-                {
-                    label = "Position",
-                    name  = "positionInput",
-                };
-                m_foldout.Add(position);
+                label = "Position",
+                name  = "positionInput",
+            };
+            m_foldout.Add(position);
 
-                var rotation = new Vector3Field
-                {
-                    label = "Rotation",
-                    name  = "rotationInput"
-                };
-                m_foldout.Add(rotation);
+            var rotation = new Vector3Field
+            {
+                label = "Rotation",
+                name  = "rotationInput"
+            };
+            m_foldout.Add(rotation);
 
-                var scale = new Vector3Field
+            var scale = new Vector3Field
+            {
+                label = "Scale",
+                name  = "scaleInput",
+                value = new Vector3(1, 1, 1)
+            };
+            m_foldout.Add(scale);
+            ///////////////// Vector3Fields /////////////////
+            
+            ///////////////// Single Transform Buttons /////////////////
+            var toggleRow = m_foldout.Q<Toggle>().Children().First();
+            toggleRow.style.paddingBottom = 3;
+            
+            var addTransformButton = new Button
+            {
+                text = "Abs",
+                style =
                 {
-                    label = "Scale",
-                    name  = "scaleInput",
-                    value = new Vector3(1, 1, 1)
-                };
-                m_foldout.Add(scale);
-            }
-
+                    right    = 0,
+                    position = Position.Absolute,
+                    width    = 30,
+                    paddingRight = 0
+                }
+            };
+            ///////////////// Single Transform Buttons /////////////////
+        
+            ///////////////// Single Transform Toggles /////////////////
+            var toggleContainer = new VisualElement
+            {
+                style =
+                {
+                    flexDirection = FlexDirection.Row,
+                    justifyContent = Justify.FlexEnd,
+                    alignItems = Align.FlexEnd,
+                }
+            };
+                
+            var isActiveCheckbox = new Toggle()
+            {
+                label = "Active",
+                labelElement =
+                {
+                    style =
+                    {
+                        minWidth = 0,
+                    }   
+                },
+                
+                name  = "isActiveCheckbox",
+                style =
+                {
+                    paddingLeft = 5
+                }
+            };
+            
             var appliedToVertexCheckbox = new Toggle
             {
                 label = "Applied To Vertices",
-                name  = "appliedToVerticesCheckbox"
-            };
-            m_foldout.Add(appliedToVertexCheckbox);
-            
-            {
-                var addTransformButton = new Button
+                labelElement =
                 {
-                    text = "Abs",
                     style =
                     {
-                        right    = 0,
-                        position = Position.Absolute
-                    }
-                };
-            
-                var isActiveCheckbox = new Toggle()
-                {
-                    text = "Active",
-                    style =
-                    {
-                        right    = 20,
-                        position = Position.Absolute
-                    },
-                };
+                        minWidth = 0,
+                    }   
+                },
                 
-                var toggleRow = m_foldout.Q<Toggle>().Children().First();
-                toggleRow.Add(addTransformButton);
-                toggleRow.Add(isActiveCheckbox);
-            }
+                name  = "appliedToVerticesCheckbox",
+                style =
+                {
+                    paddingLeft = 5
+                }
+            };
+            ///////////////// Single Transform Toggles /////////////////
+            
+            m_foldout.Add(toggleContainer);
+            toggleRow.Add(addTransformButton);
+            
+            toggleContainer.Add(isActiveCheckbox);
+            toggleContainer.Add(appliedToVertexCheckbox);
+            
+            
+            
+
         }
     }
 }
